69b36dae2406682d39f8cfdbbd7b3da6b42856b6
Tyler <tbuchhei@alumni.nd.edu>
2018-01-14 08:13:44 -0600

    Add basic .txt file appending

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-console.log("starting app");
+console.log('starting app');
 
+const fs = require('fs');
+const os = require('os');
+
+let user = os.userInfo();
+console.log(user);
+
+fs.appendFile('greetings.txt', `Hello ${user.username}!`, err => {
+  if (err) console.log('Unable to write to file');
+});
