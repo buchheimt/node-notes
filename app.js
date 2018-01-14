622d0462ff619aaee7730e497bdaf0c37dd3f92e
Tyler <tbuchhei@alumni.nd.edu>
2018-01-14 13:12:18 -0600

    Add stubbed CLI argument handling

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
 console.log('starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes');
 
-let filteredArray = _.uniq(['Tyler', 1, 2, 3, 1, 'Bob', 'Bob', 'Emma']);
-console.log(filteredArray);
+const command = process.argv[2];
+console.log(`Command: ${command}`);
+console.log(process.argv);
 
-// console.log(_.isString('hi'));
-// console.log(_.isString(true));
-
-// console.log(notes.add(7, 6));
-
-// let res = notes.addNote();
-// console.log(res);
-
-// let user = os.userInfo();
-
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`, err => {
-//   if (err) console.log('Unable to write to file');
-// });
-
+if (command === 'add') {
+  console.log('Adding new note');
+} else if (command === 'list') {
+  console.log('Listing all notes');
+} else if (command === 'read') {
+  console.log('Reading note');
+} else if (command === 'remove') {
+  console.log('Removing note');
+} else {
+  console.log('Command not recognized');
+}
--- notes.js
+++ notes.js
@@ -1,10 +1,6 @@
 console.log('starting notes.js');
 
-module.exports.age = 26;
-
 module.exports.addNote = () => {
   console.log('addNote');
   return 'New note';
 }
-
-module.exports.add = (x, y) => x + y;
