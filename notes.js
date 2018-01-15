788a66e371bffbcf99988fe1863b5ba2e26c2b97
Tyler <tbuchhei@alumni.nd.edu>
2018-01-15 11:45:18 -0600

    Implement yargs and stub notes.js functions

--- notes.js
+++ notes.js
@@ -1,6 +1,24 @@
 console.log('starting notes.js');
 
-module.exports.addNote = () => {
-  console.log('addNote');
-  return 'New note';
+const addNote = (title, body) => {
+  console.log('Adding note', title, body);
+}
+
+const getAll = () => {
+  console.log('Getting all notes');
+}
+
+const getNote = title => {
+  console.log('Getting', title);
+}
+
+const removeNote = title => {
+  console.log('Removing', title);
+}
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
 }
