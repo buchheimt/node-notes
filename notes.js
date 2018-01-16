ca8d4a8d7074b9c7da2dbe4b9f73e6b506e2c83f
Tyler <tbuchhei@alumni.nd.edu>
2018-01-16 15:06:03 -0600

    Implement getAll and clean up console logs

--- notes.js
+++ notes.js
@@ -1,5 +1,3 @@
-console.log('starting notes.js');
-
 const fs = require('fs');
 
 const fetchNotes = () => {
@@ -28,7 +26,7 @@ const addNote = (title, body) => {
 }
 
 const getAll = () => {
-  console.log('Getting all notes');
+  return fetchNotes();
 }
 
 const getNote = title => fetchNotes().find(note => note.title === title);
